6a60068001d50ca8325d1851751413a84322a4f7
nikhil yadav <iamnikhilyadav27@gmail.com>
2021-12-13 18:17:57 +0530

    chart component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Chart from "../components/Chart";
 import FeaturedInfo from "../components/FeaturedInfo";
+import { userData } from "../dummyData";
 
 const Container = styled.div`
   flex: 4;
@@ -9,6 +11,12 @@ const Home = () => {
   return (
     <Container>
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        dataKey="Active User"
+        title="User Analytics"
+        grid
+      />
     </Container>
   );
 };
